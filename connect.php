7861897ca9cc59434399b2031875d5e7bd4326db
loontan <135615278+loontan@users.noreply.github.com>
2025-01-09 15:45:11 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$host="localhost";
-$user="root";
-$pass="";
+$host="db-mysql-demo.mysql.database.azure.com";
+$user="sqladm";
+$pass="Lp@155202";
 $db="login";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
